47c6f972cd51387b311fb7cecf748eb111378f94
Emily S <emily.sugano@gmail.com>
2018-06-08 22:49:55 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,7 +34,7 @@ class Student
     SELECT * FROM students WHERE grade = ?
     SQL
 
-    DB[:conn].execute(sql,9).map do |row|
+    DB[:conn].execute(sql,"9").map do |row|
       self.new_from_db(row)
     end.first
   end
